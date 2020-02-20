74c4d47e6efac5d013ea941b1c74a9bad6e12556
Stephen Peters <7speter@gmail.com>
2020-02-20 19:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
+
+function sayHiToGrandma(string){
+  if (string === string.toUpperCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toLoweCase()) {
+    return "YES INDEED"
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
